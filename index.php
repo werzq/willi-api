b82cc905d431cac57a6d17d8d77d52b0738704d4
iamstrawberry <realkoalaficationz@gmail.com>
2023-03-12 13:01:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-$dir = __DIR__ . '/pictures';
-$favicon_path = __DIR__ . '/favicon.ico';
-$favicon_url = 'https://raw.githubusercontent.com/iamstrawberry/willi-api/main/favicon.ico';
-$last_update_file = __DIR__ . '/data.meow';
-$repo_api_url = 'https://api.github.com/repos/iamstrawberry/willi-api/contents/pictures';
-$repo_path = 'https://raw.githubusercontent.com/iamstrawberry/willi-api/main/pictures';
+$dir = __DIR__ . "/pictures";
+$favicon_path = __DIR__ . "/favicon.ico";
+$favicon_url = "https://raw.githubusercontent.com/iamstrawberry/willi-api/main/favicon.ico";
+$last_update_file = __DIR__ . "/data.meow";
+$repo_api_url = "https://api.github.com/repos/iamstrawberry/willi-api/contents/pictures";
+$repo_path = "https://raw.githubusercontent.com/iamstrawberry/willi-api/main/pictures";
 
 // Favicon check
 if (!file_exists($favicon_path)) {
+  proc_log("Getting favicon.");
   $favicon_data = file_get_contents($favicon_url);
   file_put_contents($favicon_path, $favicon_data);
 }
@@ -15,29 +16,42 @@ if (!file_exists($favicon_path)) {
 // Check if the image directory exists
 if (!file_exists($dir) || !is_dir($dir)) {
   // Create the directory
+  proc_log("Setting image directory.");
   if (!mkdir($dir, 0777, true)) {
-    die('Error: Failed to create image directory');
+    die("Error: Failed to create image directory");
   }
 }
 
 // Check if it's been less than 30 minutes since the last update
-$last_update_time = file_exists($last_update_file) ? file_get_contents($last_update_file) : 0;
+$last_update_time = file_exists($last_update_file)
+  ? file_get_contents($last_update_file)
+  : 0;
 if (time() - $last_update_time >= 1800) {
+  proc_log("Fetching images from repository.");
   // Fetch the image list from the repository API
   $context = stream_context_create([
-    'http' => ['header' => 'User-Agent: PHP']
+    "http" => ["header" => "User-Agent: PHP"],
   ]);
   $json = file_get_contents($repo_api_url, false, $context);
   $files = [];
   if ($json !== false) {
     $data = json_decode($json, true);
     foreach ($data as $item) {
-      if (isset($item['type']) && $item['type'] === 'file' && isset($item['download_url'])) {
-        $url = $item['download_url'];
-        $ext = strtolower(pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION));
-        if (in_array($ext, ['jpg', 'jpeg', 'png'])) {
-          $filename = $dir . '/' . basename($url);
-          if (!file_exists($filename) || (time() - filemtime($filename) >= 1800)) {
+      if (
+        isset($item["type"]) &&
+        $item["type"] === "file" &&
+        isset($item["download_url"])
+      ) {
+        $url = $item["download_url"];
+        $ext = strtolower(
+          pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION)
+        );
+        if (in_array($ext, ["jpg", "jpeg", "png"])) {
+          $filename = $dir . "/" . basename($url);
+          if (
+            !file_exists($filename) ||
+            time() - filemtime($filename) >= 1800
+          ) {
             // Download the image if it doesn't exist or it's been more than 30 minutes since the last download
             $image_data = file_get_contents($url, false, $context);
             if ($image_data !== false) {
@@ -53,7 +67,7 @@ if (time() - $last_update_time >= 1800) {
   file_put_contents($last_update_file, time());
 } else {
   // Get the list of image files from the directory
-  $files = glob($dir . '/*.{jpg,jpeg,png}', GLOB_BRACE);
+  $files = glob($dir . "/*.{jpg,jpeg,png}", GLOB_BRACE);
 }
 
 if (count($files) > 0) {
@@ -61,23 +75,25 @@ if (count($files) > 0) {
   $img = $files[array_rand($files)];
 
   // Check if scale and/or style arguments are provided in the URL
-  if (isset($_GET['scale']) || isset($_GET['style'])) {
+  if (isset($_GET["scale"]) || isset($_GET["style"])) {
     // Get the original image size
     $info = getimagesize($img);
 
     // Set the scale variable based on the scale parameter if it's provided
-    if (isset($_GET['scale'])) {
-      $scale = $_GET['scale'];
+    if (isset($_GET["scale"])) {
+      $scale = $_GET["scale"];
       if (preg_match('/^(\d+)%$/', $scale, $matches)) {
         $scale_value = intval($matches[1]);
         if ($scale_value < 5 || $scale_value > 500) {
-          die('wQutils@8.4.1<br>Error: Scale parameter must be between 5% and 500%');
+          die(
+            "wQutils@8.4.1<br>Error: Scale parameter must be between 5% and 500%"
+          );
         }
         $scale = $scale_value / 100;
         $height = round($info[1] * $scale);
         $width = round($info[0] * $scale);
       } else {
-        die('wQutils@8.4.1<br>Error: Invalid scale parameter');
+        die("wQutils@8.4.1<br>Error: Invalid scale parameter");
       }
     } else {
       $height = $info[1];
@@ -85,7 +101,7 @@ if (count($files) > 0) {
     }
 
     // Check if style argument is provided in the URL
-    $style = isset($_GET['style']) ? $_GET['style'] : '';
+    $style = isset($_GET["style"]) ? $_GET["style"] : "";
 
     // Calculate the aspect ratio of the original image and the resized image
     $aspect_ratio = $info[0] / $info[1];
@@ -103,17 +119,28 @@ if (count($files) > 0) {
     // Load the image and resize it to the specified dimensions
     $resized_img = imagecreatetruecolor($new_width, $new_height);
     $source_img = imagecreatefromstring(file_get_contents($img));
-    imagecopyresampled($resized_img, $source_img, 0, 0, 0, 0, $new_width, $new_height, imagesx($source_img), imagesy($source_img));
+    imagecopyresampled(
+      $resized_img,
+      $source_img,
+      0,
+      0,
+      0,
+      0,
+      $new_width,
+      $new_height,
+      imagesx($source_img),
+      imagesy($source_img)
+    );
 
     // Apply image styles if specified
-    if ($style === 'monochrome') {
+    if ($style === "monochrome") {
       imagefilter($resized_img, IMG_FILTER_GRAYSCALE);
-    } elseif ($style === 'invertedcolor') {
+    } elseif ($style === "invertedcolor") {
       imagefilter($resized_img, IMG_FILTER_NEGATE);
     }
 
     // Set the Content-Type header to the appropriate image type
-    header('Content-Type: image/jpeg');
+    header("Content-Type: image/jpeg");
 
     // Output the resized image file
     imagejpeg($resized_img);
@@ -124,14 +151,20 @@ if (count($files) > 0) {
     $info = getimagesize($img);
 
     // Set the Content-Type header to the appropriate image type
-    header('Content-Type: ' . $info['mime']);
-    header('Access-Control-Allow-Origin: *');
-    header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+    header("Content-Type: " . $info["mime"]);
+    header("Access-Control-Allow-Origin: *");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 
     // Output the image file
     readfile($img);
   }
 } else {
-  die('Error: No image files found');
+  die("Error: No image files found");
+}
+function proc_log($text)
+{
+  $timestamp = date("Y-m-d H:i:s");
+  $log_message = "[{$timestamp}] {$text}" . PHP_EOL;
+  file_put_contents(__DIR__ . "/log.meow", $log_message, FILE_APPEND);
 }
 ?>
